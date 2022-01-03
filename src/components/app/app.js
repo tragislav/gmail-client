cc1c48f212d1c3e005893ebe4d1c844c5b8e2cc8
Tragislav Home PC <s.dykomenko@gmail.com>
2022-01-04 02:40:59 +0300

    import images, styles, fonts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Header from '../header';
+
+import './styles/app.css';
+import './styles/main.less';
+import './styles/variables.less';
+
 function App() {
-    return <div>Hello!</div>;
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
 
 export default App;
