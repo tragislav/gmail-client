1154cb4e789f4eba16844d32ce435ef785eff14e
Tragislav Work PC <s.dykomenko@gmail.com>
2022-01-04 11:23:05 +0300

    import differents files from task rep

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from '../header';
+import Main from '../main';
 
-import './styles/app.css';
-import './styles/main.less';
-import './styles/variables.less';
+// import './styles/app.css';
+// import './styles/main.less';
+// import './styles/variables.less';
 
 function App() {
     return (
-        <div>
-            <Header />
-        </div>
+        <Router>
+            <div className="wrapper">
+                <Header />
+                <div>
+                    <Routes>
+                        <Route exact path="/" element={<Main />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
     );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,103 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header() {
-    return <div>Header will be here</div>;
-}
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="row">
+                    <div className="logoBlock">
+                        <Link to={'/'}>
+                            <img
+                                src="/public/images/logotype.png"
+                                className="logo"
+                                title="Logo"
+                                alt="logo"
+                            />
+                            <img
+                                src="/public/images/logotypeMobile.png"
+                                className="logoMobile"
+                                alt="logoMobile"
+                            />
+                        </Link>
+                        <span>Page title</span>
+                    </div>
+                    <div className="header-comp pull-right">
+                        <div className="dropdown">
+                            <Link
+                                to={'/'}
+                                className="dropdown-toggle dropdownMenu1"
+                                data-toggle="dropdown"
+                                aria-expanded="true"
+                            >
+                                <span>New York</span>
+                                <i className="icon-down-open"></i>
+                            </Link>
+                            <ul
+                                className="dropdown-menu"
+                                role="menu"
+                                aria-labelledby="dropdownMenu1"
+                            >
+                                <li role="presentation">
+                                    <Link
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        to={'/'}
+                                    >
+                                        Moscow
+                                    </Link>
+                                </li>
+                                <li role="presentation">
+                                    <Link
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        to={'/'}
+                                    >
+                                        Minsk
+                                    </Link>
+                                </li>
+                                <li role="presentation">
+                                    <Link
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        to={'/'}
+                                    >
+                                        Tokio
+                                    </Link>
+                                </li>
+                                <li role="presentation">
+                                    <Link
+                                        role="menuitem"
+                                        tabIndex="-1"
+                                        to={'/'}
+                                    >
+                                        Barcelona
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <Link to={'/'} className="btn btn-sm btn-header">
+                            <i className="headerIcon icon-bell"></i>
+                        </Link>
+                        <Link to={'/'} className="btn btn-sm btn-header">
+                            <i className="headerIcon icon-mail"></i>
+                        </Link>
+                        <Link to={'/'} className="profile">
+                            <span>User name</span>
+                            <img
+                                src="/public/images/profilePhoto.png"
+                                alt="profilePhoto"
+                            />
+                        </Link>
+                        <Link to={'/'} className="btn btn-xs btn-header">
+                            <i className="headerIcon icon-search"></i>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
